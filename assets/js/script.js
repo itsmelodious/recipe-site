99d0f35866a59d8d4b406ca541389fe2cfeb0c7d
Melody Yan <melodyyan@berkeley.edu>
2018-04-21 23:22:59 -0700

    Made 'See More' clickable as well

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(window).load(function()  {
 
-	$("#guide-icon").click(function() { // jQuery Function Number 1
+	$("#guide-icon, #guide-caption").click(function() { // jQuery Function Number 1
 		$("html, body").animate({ scrollTop: $("#list").offset().top }, 600); // jQuery Function Number 2 + jQuery Function Number 3
 	});
 
