e07253513c81bbee6a0ff318efa9c4b7d37cf276
Melody Yan <melodyyan@berkeley.edu>
2018-04-21 23:21:22 -0700

    Completed minimum jQuery functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,13 @@ $(window).load(function()  {
 
 	$("#guide-icon").click(function() { // jQuery Function Number 1
 		$("html, body").animate({ scrollTop: $("#list").offset().top }, 600); // jQuery Function Number 2 + jQuery Function Number 3
-	})
+	});
+
+	$("#guide-icon > svg, #guide-caption").hover(function() { // jQuery Function Number 4
+		$("#guide-caption").addClass("color-hover"); // jQuery Function Number 5
+		$("#guide-icon > svg").css("fill", "black"); // jQuery Function Number 6
+	}, function() {
+		$("#guide-caption").removeClass("color-hover"); // jQuery Function Number 7
+		$("#guide-icon > svg").css("fill", "white");
+	});
 })
